6ddb88f2599071d9cd39073a0000122c5ea35ccf
Shazzzmans <63018094+Shazzzmans@users.noreply.github.com>
2020-10-27 17:01:47 -0400

    Update IndexController.java

--- src/main/java/io/pragra/learning/demo/IndexController.java
+++ src/main/java/io/pragra/learning/demo/IndexController.java
@@ -7,6 +7,59 @@ public class IndexController {
     @GetMapping("/")
     public String getHome() {
 
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");        
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");        
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");
+        System.out.println("TESTING CODE FOR SONARQUBE");        
+        
        return "index";
     }
 }
